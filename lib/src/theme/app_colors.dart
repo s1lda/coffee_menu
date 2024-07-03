dd4b07c5f3ee0c0acfb1cec1d8336ac7643d20c1
Damir <sagalbaevdamir098@gmail.com>
2024-07-03 20:31:01 +0600

    add theme for app

---
+++ lib/src/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class CoffeeAppColors {
+  static const Color activeCategoryBackground = Colors.lightBlueAccent;
+  static const Color categoryBackground = Colors.white;
+  static const Color activeCategoryTextColor = Colors.white;
+  static const Color categoryTextColor = Colors.black;
+}
